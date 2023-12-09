033511bbfe0c57ba49817a223948748c8983658a
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-12-08 17:11:58 -0800

    Add project files.

---
+++ LearnBlazer/Data/BuzzwordProvider.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+namespace LearnBlazer.Data
+{
+    public class BuzzwordJson
+    {
+        public string[] alias { get; set; }
+        public string? logo { get; set; }
+        public string? safe_name { get; set; }
+    }
+    public class Buzzword
+    {
+        public string Name { get; set; }
+        public string SafeName { get; set; }
+        public string[] Alias { get; set; }
+        public string? Logo { get; set; }
+    }
+    public class BuzzwordProviderService
+    {
+        public static Dictionary<string,Buzzword> Buzzwords { get; private set; }
+
+        public async Task<IReadOnlyDictionary<string,Buzzword>> GetBuzzwordLookup()
+        {
+            if (Buzzwords != null)
+            {
+                return Buzzwords;
+            }
+            var buzzwordsFile = $"wwwroot/data/buzzwords.json";
+            using var fileHandle = File.OpenRead(buzzwordsFile);
+            var buzzwordsJson = await JsonSerializer.DeserializeAsync<Dictionary<string,BuzzwordJson>>(fileHandle);
+            Buzzwords = new Dictionary<string, Buzzword>();
+
+            foreach (var kvp in buzzwordsJson)
+            {
+                var name = kvp.Key;
+                var buzzword = kvp.Value;
+                Buzzwords[name] = new Buzzword()
+                {
+                    Name = name,
+                    SafeName = buzzword.safe_name != null ? buzzword.safe_name : name,
+                    Logo = buzzword.logo,
+                    Alias = buzzword.alias,
+                };
+            }
+            return Buzzwords;
+        }
+        public async Task<Buzzword> TryGetBuzzword(string name)
+        {
+            var lookup = await GetBuzzwordLookup();
+            if (lookup.TryGetValue(name, out var bw)) // Name matches
+                return bw;
+
+            name = name.ToLower();
+            foreach( var kvp in lookup)
+            {
+                bw = kvp.Value;
+                if (kvp.Key.ToLower() == name)
+                    return bw;
+                if (bw.SafeName.ToLower() == name)
+                    return bw;
+                if(bw.Alias != null)
+                    foreach (var bwa in bw.Alias)
+                        if (bwa.ToLower() == name)
+                            return bw;
+            }
+            return null;
+
+        }
+    }
+
+}
---
+++ LearnBlazer/Data/ProjectProvider.cs
@@ -0,0 +1,103 @@
+﻿using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LearnBlazer.Data
+{
+    public class ProjectJson
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string summary { get; set; }
+        public string repo { get; set; }
+        public string thumbnail { get; set; }
+        public string banner { get; set; }
+        public string[] buzzwords { get; set; }
+    }
+    public class Project
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string summary { get; set; }
+        public string repo { get; set; }
+        public string thumbnail { get; set; }
+        public string banner { get; set; }
+        public string[] description { get; set; }
+        public Buzzword[] buzzwords { get; set; }
+    }
+
+    public class ProjectProviderService
+    {
+		public static Project[] Projects { get; private set; }
+
+        private async Task<string[]> LoadDescription(string id)
+        {
+            var file = $"wwwroot/data/descriptions/{id}.txt";
+            if (!File.Exists(file)) // This could cause a race condition; but it shouldn't in 'normal' execution
+                return null; 
+            return await File.ReadAllLinesAsync(file);
+        }
+
+        public async Task<Project[]> GetProjects(BuzzwordProviderService buzzwordProvider)
+        {
+			if (Projects != null)
+			{
+				return Projects;
+			}
+			var projectsFile = $"wwwroot/data/projects.json";
+            using var fileHandle = File.OpenRead(projectsFile);
+            var projectsJson = await JsonSerializer.DeserializeAsync<ProjectJson[]>(fileHandle);
+            Projects = new Project[projectsJson.Length];
+            var buzzwordsLookup = await buzzwordProvider.GetBuzzwordLookup();
+            for(var i = 0; i < projectsJson.Length; i++)
+            {
+                var srcProj = projectsJson[i];
+                var projectBuzzwords = new Buzzword[srcProj.buzzwords.Length];
+                for (var j = 0; j < projectBuzzwords.Length; j++)
+                    projectBuzzwords[j] = buzzwordsLookup[srcProj.buzzwords[j]];
+
+                Projects[i] = new Project
+                {
+                    id = srcProj.id,
+                    name = srcProj.name,
+                    summary = srcProj.summary,
+                    repo = srcProj.repo,
+                    thumbnail = srcProj.thumbnail,
+                    banner = srcProj.banner,
+                    description = await LoadDescription(srcProj.id),
+                    buzzwords = projectBuzzwords
+                };
+                
+
+            }
+			return Projects;
+        }
+        public async Task<Project[]> GetProjectsWithTag(BuzzwordProviderService buzzwordProvider, Buzzword? tag = null)
+        {
+            var projects = await GetProjects(buzzwordProvider);
+            if (tag == null)
+                return projects;
+
+            List<Project> tagged = new();
+            foreach (var project in projects)
+            {
+                if (project.buzzwords.Contains(tag))
+                    tagged.Add(project);
+            }
+            return tagged.ToArray();
+        }
+        public async Task<Project> GetProject(BuzzwordProviderService buzzwordProvider, string id)
+        {
+            var projects = await GetProjects(buzzwordProvider);
+
+            foreach (var project in projects)
+                if (project.id == id)
+                    return project;
+
+            return null;
+        }
+    }
+}
